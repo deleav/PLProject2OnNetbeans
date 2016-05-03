e8830371d60ffaf7d585b45a31734232338e3277
deleav <a42006310@gmail.com>
2016-05-04 01:03:41 +0800

    add getStringToken & getToken & peekToken

--- main.cpp
+++ main.cpp
@@ -41,6 +41,22 @@ public:
   } // Token()
 }; // class Token
 
+class Index {
+public:
+  int mX;
+  int mY;
+
+  Index() {
+    mX = -1;
+    mY = -1;
+  } // Index()
+
+  Index( int x, int y ) {
+    mX = x;
+    mY = y;
+  } // Index
+}; // class Index
+
 class Identifier {
 public:
   string mToken;
@@ -58,32 +74,99 @@ public:
 
 class Table {
 public:
-  vector<string> mTable1; // instruction
+  vector<string> mTable1; // declare
   vector<string> mTable2; // operator
+  vector<string> mTable3; // void      -> ( params )
+  vector<string> mTable4; // if, while -> ( boolExp )
+  vector<string> mTable5; // else, do  -> { stmt }
 
   Table() {
     mTable1 = GetTable1();
     mTable2 = GetTable2();
+    mTable3 = GetTable3();
+    mTable4 = GetTable4();
+    mTable5 = GetTable5();
   } // Table()
 
 private:
   vector<string> GetTable1() {
-    string strArray[] = { "int", "char", "int" };
+    string strArray[] = { "int", "float", "char", "bool", "string" };
     vector<string> table1;
-    for ( int i = 0 ; i < 3 ; i++ )
+    for ( int i = 0 ; i < 5 ; i++ )
       table1.push_back( strArray[i] );
     return table1;
   } // GetTable1()
 
   vector<string> GetTable2() {
-    string strArray[] = { "+", "-", "*", "/", ";", "(", ")", ":",
-                          ":=", "=", "<>", ">", "<", ">=", "<=", "//" };
+    string strArray[] = { ";", "(", ")", "{", "}", "+", "-", "*", "/", "%", "<", ">", ">=", "<=", "==",
+                          "!=", "&", "^", "|", "=", "||", "&&", "+=", "-=", "*=", "/=", "%=", "++", "--",
+                          "<<", ">>", ",", "?", ":" };
     vector<string> table2;
-    for ( int i = 0 ; i < 16 ; i++ )
+    for ( int i = 0 ; i < 34 ; i++ )
       table2.push_back( strArray[i] );
     return table2;
   } // GetTable2()
+
+  vector<string> GetTable3() {
+    vector<string> table3;
+    table3.push_back( "void" );
+    return table3;
+  } // GetTable3()
+
+  vector<string> GetTable4() {
+    vector<string> table4;
+    table4.push_back( "if" );
+    table4.push_back( "while" );
+    return table4;
+  } // GetTable4()
+
+  vector<string> GetTable5() {
+    vector<string> table5;
+    table5.push_back( "else" );
+    table5.push_back( "do" );
+    return table5;
+  } // GetTable5()
 }; // class Table
+
+// /////////////////////////////////////////////////////////////////////////////
+//                                 Global                                     //
+// /////////////////////////////////////////////////////////////////////////////
+
+typedef vector<Token> OneLineToken;
+vector<OneLineToken> gAllLineToken;
+Table gTable;
+Index gIndex; // index of gAllLineToken
+
+// /////////////////////////////////////////////////////////////////////////////
+//                                 IsOO                                       //
+// /////////////////////////////////////////////////////////////////////////////
+
+bool IsTable2( string str ) {
+  for ( int i = 0 ; i < gTable.mTable2.size() ; i++ )
+    if ( str == gTable.mTable2[i] )
+      return true;
+  return false;
+} // IsTable2()
+
+bool IsANumChar( string charStr ) {
+  if ( charStr.size() == 1 && charStr >= "0" && charStr <= "9" )
+    return true;
+
+  return false;
+} // IsANumChar()
+
+bool IsEnChar( string charStr ) {
+  if ( charStr.size() == 1 &&
+       ( ( charStr >= "a" && charStr <= "z" ) || ( charStr >= "A" && charStr <= "Z" ) ) )
+    return true;
+  return false;
+} // IsEnChar()
+
+bool RecognizedIDTokenHead( string charStr ) {
+  if ( charStr == "_" || IsEnChar( charStr ) )
+    return true;
+  return false;
+} // RecognizedIDTokenHead()
 
 // /////////////////////////////////////////////////////////////////////////////
 //                                 Print                                      //
@@ -158,13 +241,28 @@ bool GetIdenToken( string oneLineString, OneLineToken &oneLineToken, int &index 
   return false;
 } // GetIdenToken()
 
-bool GetTokenString( string oneLineString, OneLineToken &oneLineToken, int &index ) {
-  // PrintNowFunction( "GetTokenString" );
-  string aCharToString, aTokenString;
-  aCharToString = string();
-  aCharToString += oneLineString[index];
-  while ( index < oneLineString.size() && )
+bool GetTokenString( string &oneLineString, OneLineToken &oneLineToken, int &index, string &aTokenString ) {
+  PrintNowFunction( "GetTokenString" );
+  string aCharToString;
+  while ( index < oneLineString.size() && aCharToString != "\"" ) {
+    aCharToString = string();
+    aCharToString += oneLineString[index];
+    aTokenString += aCharToString;
+    index++;
+  } // while
 
+  if ( index >= oneLineString.size() ) {
+    GetOneLineString( oneLineString );
+    index = 0;
+    return GetTokenString( oneLineString, oneLineToken, index, aTokenString );
+  } // if
+  else if ( aCharToString == "\"" ) { // get string
+    Token token( aTokenString, gAllLineToken.size(), oneLineToken.size() );
+    oneLineToken.push_back( token );
+    return true;
+  } // else if
+
+  return false;
 } // GetTokenString()
 
 bool SymbolOrRecognizedToken( string oneLineString, OneLineToken &oneLineToken, int &index ) {
@@ -184,23 +282,26 @@ bool SymbolOrRecognizedToken( string oneLineString, OneLineToken &oneLineToken, 
         index--;
     } // if
 
-    if ( aTokenString == ":" ) {
-      // 回報錯誤
-      // PrintUnrecognizedToken( aTokenString );
-      return false;
-    } // if
-    else if ( aTokenString == "//" ) {
-      index = oneLineString.size();
-      return true;
-    } // else if
-
     Token token( aTokenString, gAllLineToken.size(), oneLineToken.size() );
     oneLineToken.push_back( token );
     return true;
   } // if
+  else if ( aCharToString == "!" ) {
+    aTokenString += aCharToString;
+    index++;
+    if ( index < oneLineString.size() ) {
+      aCharToString += oneLineString[index];
+      if ( IsTable2( aCharToString ) ) {
+        aTokenString = aCharToString;
+        Token token( aTokenString, gAllLineToken.size(), oneLineToken.size() );
+        oneLineToken.push_back( token );
+        return true;
+      } // if
+    } // if
+  } // else if
 
   // PrintUnrecognizedToken( aCharToString );
-  cout << "SymbolOrRecognizedToken error"
+  cout << "SymbolOrRecognizedToken error";
   return false;
 } // SymbolOrRecognizedToken()
 
@@ -214,13 +315,15 @@ bool GetOneLineToken() {
     aCharToString = string();
     aCharToString += oneLineString[i];
     if ( IsANumChar( aCharToString ) || aCharToString == "." ) {
-      GetNumToken( oneLineString, oneLineToken, i );
+         GetNumToken( oneLineString, oneLineToken, i );
     } // if
     else if ( RecognizedIDTokenHead( aCharToString ) ) {
       GetIdenToken( oneLineString, oneLineToken, i );
     } // else if
     else if ( aCharToString == "\"" ) {
-      GetTokenString( oneLineString, oneLineToken, i );
+      string aTokenString = aCharToString;
+      i++;
+      GetTokenString( oneLineString, oneLineToken, i, aTokenString );
     } // else if
     else if ( aCharToString != " " && aCharToString != "\n" && aCharToString != "\t" ) {
       if ( !SymbolOrRecognizedToken( oneLineString, oneLineToken, i ) )
@@ -239,6 +342,41 @@ bool GetOneLineToken() {
   return true;
 } // GetOneLineToken()
 
+Token PeekToken() {
+  // PrintNowFunction( "PeekToken" );
+  if ( gIndex.mX > -1 )
+    if ( gIndex.mX < gAllLineToken.size() && gIndex.mY + 1 < gAllLineToken[gIndex.mX].size() ) {
+      // peek next token
+      return gAllLineToken[gIndex.mX][gIndex.mY+1];
+    } // if
+
+  // call GetOneLineToken()
+  if ( GetOneLineToken() ) {
+    gIndex.mX++;
+    gIndex.mY = -1;
+  } // if
+
+  return PeekToken();
+} // PeekToken()
+
+Token GetToken() {
+  // PrintNowFunction( "GetToken" );
+  if ( gIndex.mX > -1 )
+    if ( gIndex.mX < gAllLineToken.size() && gIndex.mY + 1 < gAllLineToken[gIndex.mX].size() ) {
+      // peek next token
+      gIndex.mY++;
+      return gAllLineToken[gIndex.mX][gIndex.mY];
+    } // if
+
+  // call GetOneLineToken()
+  if ( GetOneLineToken() ) {
+    gIndex.mX++;
+    gIndex.mY = -1;
+  } // if
+
+  return GetToken();
+} // GetToken()
+
 int main() {
   string e, testNum;
   cout << "Program starts..." << endl;
@@ -247,5 +385,9 @@ int main() {
   testNum = str1;
   uTestNum  = atof( testNum.c_str() );
 
+  while ( PeekToken().mToken != "Done" ) {
+    cout << GetToken().mToken << endl;
+  } // while
+
   return 0;
-}
+} // main()
